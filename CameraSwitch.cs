41a5c51c13f64a059c456594d1fa66a76f36e959
Victoria Sumina <32194488+1ithium@users.noreply.github.com>
2020-04-20 14:31:55 +0500

    CameraSwitch.cs
    Скрипт для смены камеры (вид от третьего лица, вид от первого лица) на C#, для Unity.

---
+++ CameraSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject CameraTP;
+    public GameObject CameraFP;
+    public bool whichCam = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp("c"))
+        {
+            if (whichCam == false)
+            {
+                whichCam = true;
+            }
+            else if (whichCam == true)
+            {
+                whichCam = false;
+            }
+        }
+        if (whichCam == false)
+        {
+            CameraFP.SetActive(true);
+            CameraTP.SetActive(false);
+        }
+
+        if (whichCam == true)
+        {
+            CameraFP.SetActive(false);
+            CameraTP.SetActive(true);
+        }
+    }
+}
